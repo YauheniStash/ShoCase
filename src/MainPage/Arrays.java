d5eec8ad17e68073ce30aa412653b691ee7953b3
YauheniStash <stashenin1983.doc@gmail.com>
2019-07-12 08:56:52 -0500

    fff

--- src/MainPage/Arrays.java
+++ src/MainPage/Arrays.java
@@ -3,7 +3,7 @@ package MainPage;
 public class Arrays {
 
 	public static void main(String[] args) {
-	String	a[][]=new String [4][2];
+	String	a[][]=new String [5][2];
 	a[0][0]="Hi";
 	a[0][1]="%";
 	a[1][0]="good";
@@ -12,6 +12,8 @@ public class Arrays {
 	a[2][1]="ed";
 	a[3][0]="di";
 	a[3][1]="edureca";
+	a[3][0]="di";
+	a[3][1]="edureca2";
 	/*for (int row=0;row<a.length;row++) {
 		for(int col=0;col<a[0].length;col++)
 			System.out.println(a[row][col]);
